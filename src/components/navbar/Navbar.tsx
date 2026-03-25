dcd31737fa54bf7250503463d759969a056ca456
georgystavissky <georgystavissky@gmail.com>
2026-03-25 23:34:54 +0300

    Add navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,43 @@
-import Link from "next/link";
 import "./navbar.scss";
 
-type NavLink = {
-  text: string;
-  href: string;
-};
+interface INavLink {
+  content: string;
+  addClass?: string;
+}
 
-const navLinks: NavLink[] = [];
+const navLinks: INavLink[] = [
+  {
+    content: "About",
+  },
+  {
+    content: "Stack",
+  },
+  {
+    content: "Experience",
+  },
+  {
+    content: "Contact",
+    addClass: "nav-cta",
+  },
+];
 
 const Navbar = () => {
-  return <nav></nav>;
+  const listEls = navLinks.map((l, i) => (
+    <li key={i}>
+      <a href={"#" + l.content.toLowerCase()} className={l.addClass ?? ""}>
+        {l.content}
+      </a>
+    </li>
+  ));
+
+  return (
+    <nav>
+      <a href="#" className="nav-logo">
+        Georgii Stavisskii
+      </a>
+      <ul className="nav-links">{listEls}</ul>
+    </nav>
+  );
 };
 
 export default Navbar;
